c19c133d401ff632fb19519e019e174aa294067f
Fatemeh Azizkhani <Fatemeh.Azizkhani@MahanAir.Aero>
2020-05-23 14:47:05 +0430

    adding paskarl's triangle and leap and hello world and two fer and panagram

---
+++ exercism/exercism-twofer.js
@@ -0,0 +1,6 @@
+//Hi I am Fatemeh Azizkhani ... This code is for 2-fer exersice in exercism
+//This is for learning javascript
+export const twoFer = (name) => {
+ return (!name)?'One for you, one for me.':`One for ${name}, one for me.`;
+
+ }
