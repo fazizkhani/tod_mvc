389002e29fbe5a048453e6442cc0d7c22a1c1bfc
fazizkhani <fazizkhani72@gmail.com>
2020-06-14 11:47:44 +0430

    clearing wrong date

--- exercism/exercism-twofer.js
+++
@@ -1,6 +0,0 @@
-//Hi I am Fatemeh Azizkhani ... This code is for 2-fer exersice in exercism
-//This is for learning javascript
-export const twoFer = (name) => {
- return (!name)?'One for you, one for me.':`One for ${name}, one for me.`;
-
- }
