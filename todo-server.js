81b59d7a50fbbaf993fe63d24c17ec6b30841080
fazizkhani <fazizkhani72@gmail.com>
2020-10-18 09:07:29 +0330

    add server side code

---
+++ todo-server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+http.createServer(function (req, res) {
+    if(req.url === "/"){
+        fs.readFile("ToDoList.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+    }
+    else if(req.url.match("\.js")){
+        var imagePath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "text/javascript"});
+        fileStream.pipe(res);
+    }
+    else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+ 
+}).listen(8080);
+
