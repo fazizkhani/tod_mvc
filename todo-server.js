6772b808dede8c96ae466dc6e19da3dc245baeb3
fazizkhani <fazizkhani72@gmail.com>
2020-11-22 20:30:55 +0330

    add serever side for upload and download

--- todo-server.js
+++ todo-server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-
 http.createServer(function (req, res) {
+    
+
     if(req.url === "/"){
         fs.readFile("ToDoList.html", "UTF-8", function(err, html){
             res.writeHead(200, {"Content-Type": "text/html"});
@@ -20,10 +21,63 @@ http.createServer(function (req, res) {
         res.writeHead(200, {"Content-Type": "text/javascript"});
         fileStream.pipe(res);
     }
-    else{
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end("No Page Found");
-    }
+	else if (req.method === "GET" && req.url === "/") {
+		
+			fs.readFile("ToDoList.html", function (error, content) {
+				if (error) {
+					res.writeHead(404, {
+						"Content-Type": "text/html"
+					});
+					res.end("404 Not Found");
+				}
+				res.writeHead(200, {
+					"Content-Type": contentType
+				});
+				res.end(content, "utf8");
+			});
+		
+    } 
+    else if (req.method === "GET" && req.url === "/read") {
+       
+			fs.readFile("ToDoList.html", function (err, data) {
+				if (err) throw err;
+				res.writeHead(200, {
+					"Content-Type": "text/html"
+				});
+				res.write(data);
+				res.end();
+			});
+		
+	} else if (req.method === "POST" && req.url === "/write") {
+	
+			req.on("data", function (data) {
+				fs.writeFile("ToDoList.html", data, function (err) {
+					if (err) throw err;
+				});
+			});
+			req.on("end", function () {
+				console.log("Saved successfully!");
+			});
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			});
+			res.end();
+		
+	} else {
+		
+			fs.readFile( req.url, function (err, content) {
+				if (err) {
+					res.writeHead(404, {
+						"Content-Type": "text/html"
+					});
+					res.end("404 Not Found");
+				}
+				res.writeHead(200);
+				res.end(content, "utf8");
+			});
+		
+	}
+
     
-}).listen(8080);
+}).listen(8081);
 
